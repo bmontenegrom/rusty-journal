bfa93124b8728b3fa90884114485f22813a7bee4
Benjamín Montenegro <benjamin.montenegrom@gmail.com>
2024-04-09 15:20:57 -0300

    finalizada

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 mod cli;
 mod tasks;
-use structopt::StructOpt;
 
-fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+use anyhow::anyhow;
+use std::path::PathBuf;
+
+use cli::{Action::*, CommandLineArgs};
+use structopt::StructOpt;
+use tasks::Task;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
+fn main() -> anyhow::Result<()>{
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .ok_or(anyhow!("Failed to find journal file."))?;
+
+    match action {
+        Add { task } => tasks::add_task(&journal_file, Task::new(task)),
+        List => tasks::list_tasks(&journal_file),
+        Done { position } => tasks::complete_task(&journal_file, position),
+    }?;
+    Ok(())
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,44 +1,85 @@
-use std::{fs::OpenOptions, io::{Result, Seek, SeekFrom}, path::PathBuf};
+use std::{
+    fmt,
+    fs::{File, OpenOptions},
+    io::{Error, ErrorKind, Result, Seek, SeekFrom},
+    path::PathBuf,
+};
 
-
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::{Deserialize, Serialize};
-use chrono::{DateTime, Utc, serde::ts_seconds, Local};
-
-
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Task{
+pub struct Task {
     pub text: String,
     #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
-impl Task{
-    pub fn new(text: String) -> Task{
-        Task{
+impl Task {
+    pub fn new(text: String) -> Task {
+        Task {
             text,
             created_at: Utc::now(),
         }
     }
 }
 
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
+    }
+}
 
-
-pub fn add_task(journal_path: &PathBuf, task: Task)->Result<()>{
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(journal_path)?;
-
-    let mut tasks = match serde_json::from_reader(&file){
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?;
+    let tasks = match serde_json::from_reader(file) {
         Ok(tasks) => tasks,
         Err(e) if e.is_eof() => Vec::new(),
         Err(e) => panic!("An error occurred: {}", e),
     };
-
     file.seek(SeekFrom::Start(0))?;
+    Ok(tasks)
+}
+
+pub fn add_task(journal_path: &PathBuf, task: Task) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+    let mut tasks = collect_tasks(&file)?;
     tasks.push(task);
     serde_json::to_writer(&file, &tasks)?;
+    Ok(())
+}
+
+pub fn complete_task(journal_path: &PathBuf, task_position: usize) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+    let mut tasks = collect_tasks(&file)?;
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "invalid task ID"));
+    }
+    tasks.remove(task_position - 1);
+    file.set_len(0)?;
+    let _ = serde_json::to_writer(file, &tasks);
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: &PathBuf) -> Result<()> {
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    let tasks = collect_tasks(&file)?;
+    if tasks.is_empty() {
+        println!("Tasks list is empty!");
+    } else {
+        let mut order = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
     Ok(())
 }
